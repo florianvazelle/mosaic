71c29c0e61b86197cae587271fb55b08cdfd43c1
florianvazelle <ponythugflorian@gmail.com>
2020-01-24 01:23:53 +0100

    Add assets

--- src/main.cpp
+++ src/main.cpp
@@ -2,14 +2,14 @@
 #include "GSImage.h"
 
 int main(int argc, char const* argv[]) {
-    if (argc != 4) {
+    /* if (argc != 4) {
         std::cout << "\n\tUsage: " << argv[0] << " <I> <dir> <R> <C> <resize> <sim>\n"
             << std::endl;
         exit(1);
-    }
+    } */
 
-    GSImage image(argv[1]);
-    //image.save_png(argv[2]);
+    GSImage image("./assets/wiki.png");
+    image.save_png("../../assets/out.png");
 
     return 0;
 }
